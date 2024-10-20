22b25c9d21237e00322dbac4d3ad9864b6ca73ba
shkim <seonflower91@gmail.com>
2024-10-20 21:50:39 +0900

    #fix: 컴포넌트 분리

--- src/views/posts/PostCreateView.vue
+++ src/views/posts/PostCreateView.vue
@@ -2,33 +2,43 @@
   <div>
     <h2>게시글 등록</h2>
     <hr class="my-4" />
-    <form @submit.prevent>
-      <div class="mb-3">
-        <label for="title" class="form-label">제목</label>
-        <input type="text" class="form-control" id="title" />
-      </div>
-      <div class="mb-3">
-        <label for="content" class="form-label">내용</label>
-        <textarea class="form-control" id="content" rows="3"></textarea>
-      </div>
-      <div class="pt-4">
-        <button
-          type="button"
-          class="btn btn-outline-dark me-2"
-          @click="goListPage"
-        >
+    <PostForm
+      v-model:title="form.title"
+      v-model:content="form.content"
+      @submit.prevent="save"
+    >
+      <template #actions>
+        <button type="button" class="btn btn-outline-dark" @click="goListPage">
           목록
         </button>
         <button class="btn btn-primary">저장</button>
-      </div>
-    </form>
+      </template>
+    </PostForm>
   </div>
 </template>
 
 <script setup>
+import { ref } from 'vue';
 import { useRouter } from 'vue-router';
+import { createPost } from '@/api/posts';
+import PostForm from '@/components/posts/PostForm.vue';
 
 const router = useRouter();
+const form = ref({
+  title: null,
+  content: null,
+});
+const save = () => {
+  try {
+    createPost({
+      ...form.value,
+      createdAt: Date.now(),
+    });
+    router.push({ name: 'PostList' });
+  } catch (error) {
+    console.error(error);
+  }
+};
 const goListPage = () => router.push({ name: 'PostList' });
 </script>
 
